ae04ed1c037b66cd60cc58fa608a105b42f951b5
atif00788 <atif00788@gmail.com>
2019-11-14 17:39:22 +0100

    implementing flatlist

--- App.js
+++ App.js
@@ -1,96 +1,101 @@
-/**
- * Demo React native app
- * https://github.com/facebook/react-native
- *
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-} from 'react-native';
 
 
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.title}>BARVIDA</Text>
-              <Text style={styles.sectionTitle}>Settings</Text>
-              <View style={styles.divider}></View>
-              <Text style={styles.sectionList}>WI-FI</Text>
-              <Text style={styles.sectionList}>Bluetooth</Text>
-              <Text style={styles.sectionList}>Flight Mode</Text>
-              <Text style={styles.sectionList}>Hotspot</Text>
-              <Text style={styles.sectionList}>Network</Text>
-              <View style={styles.divider}></View>
-              <Text style={styles.sectionList}>Sound</Text>
-              <Text style={styles.sectionList}>Alarm</Text>
-              <Text style={styles.sectionList}>Do Not Disturb</Text>
-              <View style={styles.divider}></View>
-              <Text style={styles.sectionList}>Clock</Text>
-              <Text style={styles.sectionList}>Display</Text>
-              <Text style={styles.sectionList}>Security</Text>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import React, { Component } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { ListItem, Icon  } from "react-native-elements";
 
-// Using Style elements to design UI
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    
+      Settings: [
+        {name:"Wi-Fi",
+         icon: "wifi"},
+        {name:"Bluetooth",
+        icon: "bluetooth"},
+        {name:"FlightMode",
+        icon: "airplanemode-active"},
+        {name:"Hotspot",
+        icon: "wifi-tethering"},
+        {name:"Network",
+        icon: "signal-cellular-4-bar"},
+        {name:"Sound",
+        icon: "volume-up"},
+        {name:"Alarm",
+        icon: "alarm"},
+        {name:"Do Not disturb",
+        icon: "do-not-disturb"},
+        {name:"Clock",
+        icon: "watch-later"},
+        {name:"Display",
+        icon: "video-label"},
+        {name:"Security",
+        icon: "lock"}
+      ]
+    };
+  }
+
+  
+  
+
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "88%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "12%"
+        }}
+      />
+    );
+  };
+  renderHeader = () => {
+    return <Text style={styles.title} >Settings</Text>
+    ;
+  };
+
+  
+
+  render() {
+    return (
+      
+        <FlatList
+        
+          data={this.state.Settings}
+          renderItem={({ item }) => (
+            <ListItem
+              
+              title={item.name}
+              leftIcon={{ name: item.icon }}
+
+              
+            />
+          )}
+          keyExtractor={item => item.name}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+
+         
+        />
+     
+    );
+  }
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: 'white',
-  },
-  divider: {
-    backgroundColor: 'lightgrey',
-    padding: 16,
-  },
-  body: {
-    backgroundColor: 'white',
-  },
-  sectionContainer: {
-    marginTop: 16,
-   backgroundColor: 'white',
-
-  },
+  
   title: {
     fontSize: 32,
     fontWeight: '600',
     color: 'black',
     padding: 5,
     backgroundColor: 'lightgrey',
-  },
-  sectionTitle: {
-    fontSize: 32,
-    fontWeight: '600',
-    color: 'black',
-    padding: 5,
-    backgroundColor: 'white',
-  },
-  sectionList: {
-    marginTop: 8,
-    paddingLeft: 5,
-    fontSize: 24,
-    fontWeight: '400',
-    color: 'black',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-
+  }
 });
 
-export default App;
+
