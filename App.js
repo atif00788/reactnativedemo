2a4c00c706caa9e75604d36c68d30e1b48be682d
atif00788 <atif00788@gmail.com>
2019-11-14 18:05:01 +0100

    Implementing FlatList

--- App.js
+++ App.js
@@ -3,6 +3,7 @@
 
 import React, { Component } from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
+//importing icons and listItem
 import { ListItem, Icon  } from "react-native-elements";
 
 export default class App extends React.Component {
@@ -10,7 +11,7 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-    
+    // creating data array to pass to flatlist
       Settings: [
         {name:"Wi-Fi",
          icon: "wifi"},
@@ -54,6 +55,8 @@ export default class App extends React.Component {
       />
     );
   };
+
+  // Function to display title or header of flatlist
   renderHeader = () => {
     return <Text style={styles.title} >Settings</Text>
     ;
@@ -69,13 +72,13 @@ export default class App extends React.Component {
           data={this.state.Settings}
           renderItem={({ item }) => (
             <ListItem
-              
+
               title={item.name}
               leftIcon={{ name: item.icon }}
 
-              
             />
           )}
+          // adding a key value to the list elements
           keyExtractor={item => item.name}
           ItemSeparatorComponent={this.renderSeparator}
           ListHeaderComponent={this.renderHeader}
